fd081561c1d7f8c3131b41e5dfe9ca8df1f10c32
TiantianYin <ty2346@columbia.edu>
2018-10-03 12:10:37 -0700

    Update gbn.c

--- gbn.c
+++ gbn.c
@@ -185,7 +185,7 @@ RECV:
 		make_packet(&rec_header, DATAACK, s.rec_seqnum, 0, NULL, 0);
 		if (sendto(sockfd, &rec_header, sizeof(rec_header), 0, &cli, cli_len) == -1) {
 			printf ("error sending in gbn_recv\n");
-			return -1;
+			goto RECV;
 		}
 		printf("sent dataack with seqnum %i\n", s.rec_seqnum);
 		/* if successfully send ACK, expected next rec_seqnum ++ */
