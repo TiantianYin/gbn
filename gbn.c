75bdc3f1549fe1a0b7f685c332ab91c3e0442e95
TiantianYin <ty2346@columbia.edu>
2018-10-02 22:19:30 -0700

    Update gbn.c

--- gbn.c
+++ gbn.c
@@ -428,8 +428,9 @@ int gbn_accept(int sockfd, struct sockaddr *client, socklen_t *socklen){
 			s.state = ESTABLISHED;
 			printf("receiver connection established\n");
 			gbnhdr sender_packet;
-			gbnhdr* sender_p = &sender_packet;
-			if (recvfrom(sockfd, (char *)sender_p, sizeof(*sender_p), 0, tmp, tmp_int) != -1) {
+			
+			if (recvfrom(sockfd, (char *)&sender_packet, sizeof(sender_packet), 0, tmp, tmp_int) != -1) {
+				gbnhdr* sender_p = &sender_packet;
 				printf("got type:%d, data: %s\n", sender_p->type, sender_p->data);
 			} else {
 				printf("error in gbn_recv new pl1\n");
