1b80afe2061aed31e13c7845cfa1aa2d931603a6
TiantianYin <ty2346@columbia.edu>
2018-10-02 22:27:52 -0700

    Update gbn.c

--- gbn.c
+++ gbn.c
@@ -427,8 +427,8 @@ int gbn_accept(int sockfd, struct sockaddr *client, socklen_t *socklen){
 			printf("receiver received synack header\n");
 			s.state = ESTABLISHED;
 			printf("receiver connection established\n");
-			gbnhdr sender_packet;
 /*
+			gbnhdr sender_packet;
 LALALA:
 			if (recvfrom(sockfd, (char *)&sender_packet, sizeof(sender_packet), 0, tmp, tmp_int) != -1) {
 				printf("got type:%d, data: %s\n", sender_packet.type, sender_packet.data);
