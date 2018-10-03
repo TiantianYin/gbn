0e1de30d6dbe691e4e69a1aa6dbd650d07531b2d
TiantianYin <ty2346@columbia.edu>
2018-10-03 01:09:39 -0700

    Update gbn.h

--- gbn.h
+++ gbn.h
@@ -246,7 +246,7 @@ ssize_t gbn_recv(int sockfd, void *buf, size_t len, int flags){
 RECVAGAIN:
     received = &tmpbuf;
     recvfrom(sockfd, &tmpbuf, sizeof(tmpbuf), 0, &si_tmp, &tmpsocklen);
-    printf("received type: %d\n", received->type);
+    printf("received type: %d\n", tmpbuf.type);
     goto RECVAGAIN;
     return 0;
 }
