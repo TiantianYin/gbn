7a45259e2ab4236ca69319ca2a42b1a2c34c4cbd
TiantianYin <ty2346@columbia.edu>
2018-10-02 21:55:15 -0700

    Update gbn.c

--- gbn.c
+++ gbn.c
@@ -428,6 +428,19 @@ int gbn_accept(int sockfd, struct sockaddr *client, socklen_t *socklen){
 			printf("receiver received synack header\n");
 			s.state = ESTABLISHED;
 			printf("receiver connection established\n");
+			gbnhdr sender_header;
+			if (recvfrom(sockfd, (char *)&sender_packet, sizeof(sender_packet), 0, tmp, tmp_int) != -1) {
+				printf("got type:%d, data: %s\n", sender_packet.type, sender_packet.data);
+			}
+			if (recvfrom(sockfd, (char *)&sender_packet, sizeof(sender_packet), 0, tmp, tmp_int) != -1) {
+				printf("got type:%d, data: %s\n", sender_packet.type, sender_packet.data);
+			}
+			if (recvfrom(sockfd, (char *)&sender_packet, sizeof(sender_packet), 0, tmp, tmp_int) != -1) {
+				printf("got type:%d, data: %s\n", sender_packet.type, sender_packet.data);
+			}
+			if (recvfrom(sockfd, (char *)&sender_packet, sizeof(sender_packet), 0, tmp, tmp_int) != -1) {
+				printf("got type:%d, data: %s\n", sender_packet.type, sender_packet.data);
+			}
 			return 0;
 		}
 		printf("received non-synack\n");
